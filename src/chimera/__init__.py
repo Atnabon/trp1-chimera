65b51f83fd4a3db5b79c2832e16928c8da1404b1
atnabon <atnabon@eaglelionsystems.com>
2026-02-05 10:37:20 +0300

    Add initial implementation files for Project Chimera

---
+++ src/chimera/__init__.py
@@ -0,0 +1,9 @@
+"""
+Project Chimera - Autonomous Influencer Network
+
+This package will contain the core implementation.
+Currently in specification phase - implementation follows TDD approach.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Chimera Team"
